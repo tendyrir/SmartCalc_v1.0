f5cc1f25ddffe3cc101609a5ac1276ecef43610e
Tendyris Ragnor <tendyrir@ga-t6.msk.21-school.ru>
2022-08-30 17:25:29 +0300

    nothing changed

--- src/main.c
+++ src/main.c
@@ -1,20 +1,22 @@
 #include "calc.h"
 
 int main(void) {
-    int arr[] = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
-    Stack *intstack = stack_create(sizeof(int));
+    char arr[255] = "1+2*3/4";
+    Stack *intstack = stack_create(sizeof(char));
 
-    for (int i = 0; i < 10; i++) {
+    for (int i = 0; i < (int) strlen(arr); i++) {
         stack_push(intstack, arr + i);  // 2nd arg is a pointer!
-        printf("pushed: %d\n", arr[i]);
+        printf("pushed: %c ", arr[i]);
     }
+        printf("\n");
 
-    int popped_int;  // storage for next pooped element
+    char popped_int;  // storage for next pooped element
 
     // we must pop all elements from the stack to avoid leaks
     while (stack_pop(intstack, &popped_int)) {
-        printf("popped: %d\n", popped_int);
+        printf("popped: %c ", popped_int);
     }
+    printf("\n");
     // and then free(Stack);
     free(intstack);
 
